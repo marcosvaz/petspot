5d523f832d2ab6aa43e4d3140449a1a41e66f170
Marcos Vaz <marcosvazdeveloper@gmail.com>
2018-05-22 17:19:14 -0300

    Atualizado link home do inicial.php

--- inicial.php
+++ inicial.php
@@ -31,7 +31,7 @@
     <nav class="navbar navbar-expand-lg bg-theme fixed-top navbar-transparent" color-on-scroll="300">
       <div class="container">
         <div class="navbar-translate">
-          <a class="navbar-brand" href="index.php"> Mascot Match </a>
+          <a class="navbar-brand" href="inicial.php"> Mascot Match </a>
           <button class="navbar-toggler navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
     					<span class="navbar-toggler-bar bar1"></span>
     					<span class="navbar-toggler-bar bar2"></span>
