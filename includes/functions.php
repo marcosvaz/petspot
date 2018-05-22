3f3b72517dcebf0537514f4772a1538f18a358a1
Marcos Vaz <marcosvazdeveloper@gmail.com>
2018-05-22 16:29:44 -0300

    Adicionada criptografia na senha

--- includes/functions.php
+++ includes/functions.php
@@ -3,6 +3,14 @@
   include 'db.php';
   include 'erros.php';
 
+  // Função de criptografia da senha do usuário
+  function criptografar($senha)
+  {
+    $salt = md5($senha);
+    $criptografada = hash('whirlpool', $salt);
+    return $criptografada;
+  }
+
   // Função de cadastro de usuário
   function registrar()
   {
@@ -15,10 +23,10 @@
 
       if (strlen( trim( $nome ) ) < 5) {
         echo danger("Informe o nome corretamente!");
-      } elseif (strlen( trim( $email ) ) < 8 && strstr("@", $email)) {
+      } elseif (strlen( trim( $email ) ) < 8 || !strstr($email, "@") || !strstr($email, ".com")) {
         echo danger("Informe corretamente seu email!");
       } elseif ( strlen(trim( $senha ) ) < 6){
-        echo danger("Informe uma senha com <strong>mais de 6 caracteres</strong>!");
+        echo danger("Informe uma senha com <strong>mais de 6 caracteres</strong>! ");
       } elseif (strlen( trim( $confirmarsenha ) ) < 6) {
         echo danger("Informe corretamente a confirmação da senha!");
       } else {
@@ -31,14 +39,15 @@
           echo warning("Este email já está cadastrado!");
         } else {
           if($senha == $confirmarsenha){
-            $query = "INSERT INTO usuario (nome, email, senha) VALUES ('$nome', '$email', '$senha')";
+            $criptografada = criptografar($senha);
+            $query = "INSERT INTO usuario (nome, email, senha) VALUES ('$nome', '$email', '$criptografada')";
             $resultado = mysqli_query($connection, $query);
             if (!$resultado) {
               die('Não deu certo!');
             } else {
               session_start();
               $_SESSION['email'] = $email;
-              $_SESSION['senha'] = $senha;
+              $_SESSION['senha'] = $criptografada;
               header('Location: inicial.php');
             }
           } else {
@@ -56,6 +65,7 @@
     if (isset($_POST['logar'])) {
       $email = $_POST['email'];
       $senha = $_POST['senha'];
+      $criptografada = criptografar($senha);
 
       $query = "SELECT * FROM usuario WHERE email = '$email'";
       $select_usuario = mysqli_query($connection, $query);
@@ -63,12 +73,12 @@
         $db_email = $row['email'];
         $db_senha = $row['senha'];
       }
-       if ((isset($db_email) && isset($db_senha)) && ($email == $db_email && $senha == $db_senha)) {
+       if ((isset($db_email) && isset($db_senha)) && ($email == $db_email && $criptografada == $db_senha)) {
         session_start();
         $_SESSION['email'] = $email;
-        $_SESSION['senha'] = $senha;
+        $_SESSION['senha'] = $criptografada;
         header('Location: inicial.php');
-      } elseif ((isset($db_email) && isset($db_senha)) && ($email != $db_email || $senha != $db_senha)) {
+      } elseif ((isset($db_email) && isset($db_senha)) && ($email != $db_email || $criptografada != $db_senha)) {
         echo warning("Email / senha incorretos!");
       } else {
         echo danger("Email não encontrado!");
