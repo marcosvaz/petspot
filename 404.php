3d6a77438b83e2179efee567ded8a5a1aaf7833a
Marcos Vaz <marcosvazdeveloper@gmail.com>
2018-05-21 22:49:07 -0300

    Alterada cor do link na pagina 404

--- 404.php
+++ 404.php
@@ -52,7 +52,7 @@
             <div class="ml-auto mr-auto">
               <h1 class="title">Ops, parece que você caiu em uma página que não existe!</h1>
               <h4 class="description">
-                <a href="index.php">
+                <a href="index.php" style="color: white;">
                   Clique aqui para ir à Página Inicial
                 </a>
               </h4>
