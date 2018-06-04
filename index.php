cb5f820568a2ac7ceeafff09f3f5526f46257c7e
Marcos Vaz <marcosvazdeveloper@gmail.com>
2018-06-04 08:16:32 -0300

    Alterado logo na index.php de apresentacao

--- index.php
+++ index.php
@@ -31,7 +31,7 @@
     <nav class="navbar navbar-expand-lg bg-theme fixed-top navbar-transparent" color-on-scroll="300">
       <div class="container">
         <div class="navbar-translate">
-          <a class="navbar-brand" href="index.php"> <img src="img/petstop-white.png" width="116px" /> </a>
+          <a class="navbar-brand" href="index.php"> <img src="img/logo-text-white.png" width="116px" /> </a>
         </div>
       </div>
     </nav>
